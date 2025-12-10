bc455373094a36da7fe2e0e113427363b0002b6e
SecondPretender <peterm03cg@gmail.com>
2025-12-10 11:51:58 -0600

    add config.properties to resources

--- expense_apps/manager/src/main/java/com/revature/repository/DatabaseConnection.java
+++ expense_apps/manager/src/main/java/com/revature/repository/DatabaseConnection.java
@@ -1,8 +1,12 @@
 package com.revature.repository;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * Database connection utility for SQLite database.
@@ -10,10 +14,30 @@ import java.sql.SQLException;
  */
 public class DatabaseConnection {
     private final String databasePath;
+
     
     public DatabaseConnection() {
+
+        try (InputStream input = new FileInputStream("config.properties")) {
+
+            Properties prop = new Properties();
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            System.setProperty("databasePath", prop.getProperty("databasePath"));
+            //testing print statement
+            System.out.println(prop.getProperty("databasePath"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+
         // Use environment variable or default path
-        System.setProperty("databasePath", "C:\\Users\\oanht\\Downloads\\Stuff for Jeremiah\\Revature QEA Training\\Miscellaneous\\P1\\expense_apps\\employee\\expense_manager.db");
+        //System.setProperty("databasePath", "C:\\Users\\peter\\OneDrive\\Desktop\\Desktop\\QEA\\CodeSilver-P1\\expense_apps\\employee\\expense_manager.db");
+        //TODO: validate this works if the property is broken
         this.databasePath = System.getenv("DATABASE_PATH") != null
             ? System.getenv("DATABASE_PATH")
             : System.getProperty("databasePath");
