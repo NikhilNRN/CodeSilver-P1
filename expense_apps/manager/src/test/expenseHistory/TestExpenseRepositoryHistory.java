13a0fe9d547b78a56f0d4e8cbcd6571855ea0df9
JeremiahTrinh <demiahtrinh725@gmail.com>
2025-12-12 15:13:13 -0600

    Updated pom.xml to include Mockito dependencies

---
+++ expense_apps/manager/src/test/expenseHistory/TestExpenseRepositoryHistory.java
@@ -0,0 +1,12 @@
+package expenseHistory;
+
+import com.revature.repository.DatabaseConnection;
+import com.revature.repository.ExpenseRepository;
+import org.mockito.Mock;
+
+public class TestExpenseRepositoryHistory {
+    @Mock
+    private static DatabaseConnection conn;
+
+    private static ExpenseRepository repo;
+}
