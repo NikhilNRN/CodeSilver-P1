305117f0da8207696f11d4c83e81aab749dbbe0a
JeremiahTrinh <89665166+JeremiahTrinh@users.noreply.github.com>
2025-12-15 19:57:09 -0600

    Wrote out test cases for TestExpenseServiceApproval.java

--- expense_apps/manager/src/test/expenseApproval/TestExpenseServiceApproval.java
+++ expense_apps/manager/src/test/expenseApproval/TestExpenseServiceApproval.java
@@ -1,7 +1,6 @@
 package expenseApproval;
 
-import com.revature.repository.ApprovalRepository;
-import com.revature.repository.ExpenseRepository;
+import com.revature.repository.*;
 import com.revature.service.ExpenseService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,6 +9,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestExpenseServiceApproval {
     @Mock
@@ -21,6 +22,13 @@ public class TestExpenseServiceApproval {
     private static ExpenseService service;
 
     // Test vars
+    private static int existingExpenseId;
+    private static int notRealExpenseId;
+    private static int existingManagerId;
+    private static int notRealManagerId;
+    private static String comment;
+
+    private boolean actualResult;
 
     @BeforeAll
     public static void setUp() {
@@ -28,7 +36,22 @@ public class TestExpenseServiceApproval {
         approvalDAO = mock(ApprovalRepository.class);
         service = new ExpenseService(expenseDAO, approvalDAO);
 
-        // Instantiate other static test vars
+        existingExpenseId = 4;
+        existingManagerId = 1;
+        notRealExpenseId = 999;
+        notRealManagerId = -5;
+        String approve = "approved";
+        String deny = "denied";
+        comment = "Some words";
+
+        when(approvalDAO.updateApprovalStatus(existingExpenseId, approve, existingManagerId, comment)).thenReturn(true);
+        when(approvalDAO.updateApprovalStatus(existingExpenseId, approve, existingManagerId, null)).thenReturn(true);
+        when(approvalDAO.updateApprovalStatus(existingExpenseId, deny, existingManagerId, comment)).thenReturn(true);
+        when(approvalDAO.updateApprovalStatus(existingExpenseId, deny, existingManagerId, null)).thenReturn(true);
+        when(approvalDAO.updateApprovalStatus(notRealExpenseId, approve, existingExpenseId, comment)).thenReturn(false);
+        when(approvalDAO.updateApprovalStatus(notRealExpenseId, deny, existingExpenseId, comment)).thenReturn(false);
+        when(approvalDAO.updateApprovalStatus(existingExpenseId, approve, notRealManagerId, comment)).thenReturn(false);
+        when(approvalDAO.updateApprovalStatus(existingExpenseId, deny, notRealManagerId, comment)).thenReturn(false);
     }
 
     @AfterAll
@@ -36,51 +59,59 @@ public class TestExpenseServiceApproval {
         //
     }
 
-    //
+    // TC-C20_01
     @Test
     public void testApproveExpense_normal_returnTrue() {
-        //
+        actualResult = service.approveExpense(existingExpenseId, existingManagerId, comment);
+        assertTrue(actualResult);
     }
 
-    //
+    // TC-C20_02
     @Test
     public void testApproveExpense_noComment_returnTrue() {
-        //
+        actualResult = service.approveExpense(existingExpenseId, existingManagerId, null);
+        assertTrue(actualResult);
     }
 
-    //
+    // TC-C20_03
     @Test
     public void testApproveExpense_invalidExpense_returnFalse() {
-        //
+        actualResult = service.approveExpense(notRealExpenseId, existingManagerId, comment);
+        assertFalse(actualResult);
     }
 
-    //
+    // TC-C20_04
     @Test
     public void testApproveExpense_invalidManager_returnFalse() {
-        //
+        actualResult = service.approveExpense(existingExpenseId, notRealManagerId, comment);
+        assertFalse(actualResult);
     }
 
-    //
+    // TC-C21_01
     @Test
     public void testDenyExpense_normal_returnTrue() {
-        //
+        actualResult = service.denyExpense(existingExpenseId, existingManagerId, comment);
+        assertTrue(actualResult);
     }
 
-    //
+    // TC-C21_02
     @Test
     public void testDenyExpense_noComment_returnTrue() {
-        //
+        actualResult = service.denyExpense(existingExpenseId, existingManagerId, null);
+        assertTrue(actualResult);
     }
 
-    //
+    // TC-C21_03
     @Test
     public void testDenyExpense_invalidExpense_returnFalse() {
-        //
+        actualResult = service.denyExpense(notRealExpenseId, existingManagerId, comment);
+        assertFalse(actualResult);
     }
 
-    //
+    // TC-C21_04
     @Test
     public void testDenyExpense_invalidManager_returnFalse() {
-        //
+        actualResult = service.denyExpense(existingExpenseId, notRealManagerId, comment);
+        assertFalse(actualResult);
     }
 }
