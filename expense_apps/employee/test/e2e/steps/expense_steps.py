f8907c1436192b74f4cd176c89c4eea3bc65212d
JeremiahTrinh <89665166+JeremiahTrinh@users.noreply.github.com>
2025-12-31 12:46:21 -0600

    Updated conftest.py, expense_steps.py, and added allure tags to my Python tests

--- expense_apps/employee/test/e2e/steps/expense_steps.py
+++ expense_apps/employee/test/e2e/steps/expense_steps.py
@@ -14,7 +14,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
